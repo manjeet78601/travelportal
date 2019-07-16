199c94fb080214ccf5fa4eedba7aec0607dfb7a9
manjeet78601 <manjeetpathania13@gmail.com>
2019-07-16 14:59:27 +0530

    image-service

--- src/app/shared/image.service.ts
+++ src/app/shared/image.service.ts
@@ -10,36 +10,59 @@ export class ImageService {
 
   }
   getImage(id: number){
-    return IMAGES.slice(0).find(image => image.id === id);
+    return IMAGES.slice(0).find(image => image.id === id)
   }
 
 
 }
 
 const IMAGES = [
-  {'id': 1, 'category': 'All tourism Places', 'caption': 'view from boat', 'url': 'assets/images/himachal12.jpg'},
+  {'id': 1, 'category': 'all', 'caption': 'view from boat', 'url': 'assets/images/himachal12.jpg'},
 
-  {'id': 2, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/banner.jpg'},
-  {'id': 3, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/him.jpg'},
-  {'id': 4, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/himachal.jpg'},
-  {'id': 5, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/kalpa.jpg'},
+  {'id': 2, 'category': 'himachal', 'caption': 'view from boat', 'url': 'assets/images/banner.jpg'},
+  {'id': 3, 'category': 'Jammu&Kashmir', 'caption': 'view from boat', 'url': 'assets/images/him.jpg'},
+  {'id': 4, 'category': 'Uttarakhand', 'caption': 'view from boat', 'url': 'assets/images/himachal.jpg'},
+  {'id': 5, 'category': '', 'caption': 'view from boat', 'url': 'assets/images/kalpa.jpg'},
 
   {'id': 6, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/killerpangichamba.jpg'},
 
   {'id': 7, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/manikaransahib.jpg'},
 
-  // {'id': 8, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 8, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/triund.jpg'},
 
-  // {'id': 9, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 9, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/uttarkhand.jpg'},
 
-  // {'id': 10, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 1, 'category': 'all', 'caption': 'view from boat', 'url': 'assets/images/himachal12.jpg'},
 
-  // {'id': 11, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 2, 'category': 'himachal', 'caption': 'view from boat', 'url': 'assets/images/banner.jpg'},
+  {'id': 3, 'category': 'Jammu&Kashmir', 'caption': 'view from boat', 'url': 'assets/images/him.jpg'},
+  {'id': 4, 'category': 'Uttarakhand', 'caption': 'view from boat', 'url': 'assets/images/himachal.jpg'},
+  {'id': 5, 'category': '', 'caption': 'view from boat', 'url': 'assets/images/kalpa.jpg'},
 
-  // {'id': 12, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 6, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/killerpangichamba.jpg'},
 
-  // {'id': 13, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 7, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/manikaransahib.jpg'},
 
-  // {'id': 14, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/img/boats.jpg'},
+  {'id': 8, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/triund.jpg'},
+
+  {'id': 9, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/uttarkhand.jpg'},
+
+
+  {'id': 1, 'category': 'all', 'caption': 'view from boat', 'url': 'assets/images/himachal12.jpg'},
+
+  {'id': 2, 'category': 'himachal', 'caption': 'view from boat', 'url': 'assets/images/banner.jpg'},
+  {'id': 3, 'category': 'Jammu&Kashmir', 'caption': 'view from boat', 'url': 'assets/images/him.jpg'},
+  {'id': 4, 'category': 'Uttarakhand', 'caption': 'view from boat', 'url': 'assets/images/himachal.jpg'},
+  {'id': 5, 'category': '', 'caption': 'view from boat', 'url': 'assets/images/kalpa.jpg'},
+
+  {'id': 6, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/killerpangichamba.jpg'},
+
+  {'id': 7, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/manikaransahib.jpg'},
+
+  {'id': 8, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/triund.jpg'},
+
+  {'id': 9, 'category': 'boats', 'caption': 'view from boat', 'url': 'assets/images/uttarkhand.jpg'},
+
+
 
 ];
